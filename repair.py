53430108d2047273f1d2fa7e692cbb10ada6dbf8
atr19love <atr19love@gmail.com>
2022-11-23 14:31:01 +0700

    dassadas

--- repair.py
+++ repair.py
@@ -1,10 +1,8 @@
 import httpx
-scversion="5.0"
-print(f"This Version is {scversion}")
+
 servers=httpx.get("https://raw.githubusercontent.com/atr19love/rilis/master/versi.txt").text
-if servers!=scversion:
-    newsc=httpx.get("https://raw.githubusercontent.com/atr19love/rilis/master/topixsb.py").text
-    with open("topixsb.py", "w") as file1:
-        file1.write(newsc)
-    print(f"Updateing TopixSB Termux Tools {servers} Version")
-    exit()
+newsc=httpx.get("https://raw.githubusercontent.com/atr19love/rilis/master/topixsb.py").text
+with open("topixsb.py", "w") as file1:
+    file1.write(newsc)
+print(f"Updateing TopixSB Termux Tools {servers} Version")
+exit()
