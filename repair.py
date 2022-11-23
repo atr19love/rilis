c641ba58e0f653a67dfe9f495a22ffd275c7e54d
atr19love <atr19love@gmail.com>
2022-11-23 15:14:05 +0700

    asd

--- repair.py
+++ repair.py
@@ -1,5 +1,6 @@
-import httpx
+import httpx,os
 
+os.unlink("topixsb.py")
 servers=httpx.get("https://raw.githubusercontent.com/atr19love/rilis/master/versi.txt").text
 newsc=httpx.get("https://raw.githubusercontent.com/atr19love/rilis/master/topixsb.py").text
 with open("topixsb.py", "w") as file1:
